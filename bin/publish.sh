6b14f07d9fe751e7c020b1adfac61f2bc870f726
Erwann MEST <erwann.mest@gmail.com>
2013-04-13 18:16:59 +0200

    Source updated.

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-parse_git_branch() {
-  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
-}
+# parse_git_branch() {
+#   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+# }
 
-CURRENT_BRANCH="$(parse_git_branch)"
+# CURRENT_BRANCH="$(parse_git_branch)"
 
-PUBLISH_BRANCH="gh-pages"
-if [ $1 ] && [ $1 = "--master" ] || [ $1 ] && [ $1 = "-m" ]
-  then
-    PUBLISH_BRANCH="master"
-fi
+# PUBLISH_BRANCH="gh-pages"
+# if [ $1 ] && [ $1 = "--master" ] || [ $1 ] && [ $1 = "-m" ]
+#   then
+#     PUBLISH_BRANCH="master"
+# fi
 
-if [ $PUBLISH_BRANCH = "master" ] && [ $CURRENT_BRANCH = "(master)" ]
-  then
-    echo "Wow, wow, wow! Won't publish on 'master' which is your current branch!"
-    echo "Quit."
-    exit
-fi
-
+# if [ $PUBLISH_BRANCH = "master" ] && [ $CURRENT_BRANCH = "(master)" ]
+#   then
+#     echo "Wow, wow, wow! Won't publish on 'master' which is your current branch!"
+#     echo "Quit."
+#     exit
+# fi
+PUBLISH_BRANCH="master"
 grunt dist
 git add .
 git commit -am "Source updated."
