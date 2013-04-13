dc6e48cc7ae70a212afefc1cd8a7212d779cd7b9
Erwann MEST <erwann.mest@gmail.com>
2013-04-13 18:23:35 +0200

    Source updated.

--- src/assets/_scripts/script.js
+++ src/assets/_scripts/script.js
@@ -1,6 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
     var email = document.getElementById('email');
     email.href = ['mailto', ':', 'm', '@', 'kud', '.', 'io'].join('');
-
-    if(!window.location.hash) { window.location = "/#myself"; }
 });
