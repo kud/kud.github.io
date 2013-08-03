e0057e99ed5eac1ce8ddf364d8480407ff6c2d08
Erwann Mest <erwann.mest@gmail.com>
2013-08-03 16:16:17 +0200

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,15 @@
 /*! _kud.profile - v1.0.0 - 2013-08-03 */
 document.addEventListener('DOMContentLoaded', function () {
-    var email = document.getElementById('kud-js-Contact');
+    var email = document.getElementById('kud-Email');
     email.href = ['mailto', ':', 'm', '@', 'kud', '.', 'io'].join('');
+
+    var aboutLink = document.getElementById('kud-AboutLink'),
+        about = document.getElementById('kud-About');
+
+    aboutLink.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      this.className = this.className + " kud-Header--alternative";
+      about.className = about.className.replace(/kud-About--hidden/g, '');
+    });
 });
