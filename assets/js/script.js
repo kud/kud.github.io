9928354f599ed1b8cbb0120046e88b253a651c06
Erwann Mest <erwann.mest@gmail.com>
2013-08-03 02:04:52 +0200

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/*! _kud.profile - v1.0.0 - 2013-08-02 */
+/*! _kud.profile - v1.0.0 - 2013-08-03 */
 document.addEventListener('DOMContentLoaded', function () {
     var email = document.getElementById('kud-js-Contact');
     email.href = ['mailto', ':', 'm', '@', 'kud', '.', 'io'].join('');
