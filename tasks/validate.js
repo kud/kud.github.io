ea583f899bc40cf983cea0f27070474bd6709c4f
Erwann Mest <m@kud.io>
2015-03-29 19:31:00 +0200

    Do not check demo.

--- tasks/validate.js
+++ tasks/validate.js
@@ -3,7 +3,7 @@ module.exports = function() {
   var gulp   = require('gulp')
   var eslint = require('gulp-eslint')
 
-  return gulp.src( 'src/**/*.js ')
+  return gulp.src( ['src/**/*.js', '!src/demo/**/*.js'] )
     .pipe( eslint() )
     .pipe( eslint.format() )
     .pipe( eslint.failOnError() )
