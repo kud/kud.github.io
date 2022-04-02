c556259d7f884a390104493e8bbf6d29aa20d9c0
Erwann Mest <m@kud.io>
2022-04-02 19:41:57 +0200

    weeeeeeeeeeeeeee

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -1,9 +1,3 @@
-import "normalize.css/normalize.css"
-
-import "~/styles/box-sizing.css"
-import "~/styles/define-rem.css"
-import "~/styles/site.css"
-
 document.addEventListener("DOMContentLoaded", function () {
   var email = document.querySelector(".kud-js-Email")
   email.href = ["mailto", ":", "m+site", "@", "kud", ".", "io"].join("")
